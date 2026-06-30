f81e3edb16b36dc85fad7cc74acac91c7c450e4d
Eckhardt <eki@example.com>
2026-06-30 09:10:03 +0200

    featuring:Favicon&text

--- script.js
+++ script.js
@@ -53,3 +53,16 @@ if (bookingForm && formMessage) {
     bookingForm.reset();
   });
 }
+
+const scrollToTopButton = document.querySelector("#scroll-to-top");
+if (scrollToTopButton) {
+  scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+}
+
+/*<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />*/
